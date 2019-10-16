e7fc36fe89bf4166e0c4f7ba6c8213ede6422255
MinhLun2498 <nguyenkhacbaominh@gmail.com>
2019-10-16 08:17:58 +0700

    Chapter 8: Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "users.create.welcome"
       redirect_to @user
     else
@@ -25,6 +26,6 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit :name,
-      :email, :password, :password_confirmation
+                                 :email, :password, :password_confirmation
   end
 end
